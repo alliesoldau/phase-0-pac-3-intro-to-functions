e4cfead2919b80a98dbfee52513c392838d9e877
Allie Soldau <alliesoldau@gmail.com>
2022-11-02 17:43:13 -0400

    final commit

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
 // Follow along with the examples here
+function tipCalc(sum, percentage) {
+    return total = sum*(percentage/100 + 1); 
+}
+
+console.log(`You owe your server a grand total of $${tipCalc(10, 20)}`);
